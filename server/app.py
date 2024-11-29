04eea31e19ae6b6f36c265bd8a6f7a2370fec153
EstherNgeno <esther.ngeno@student.moringaschool.com>
2024-11-29 19:44:02 +0300

    final commit

--- server/app.py
+++ server/app.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
-from flask import request, session
-from flask_restful import Resource
+from flask import request, session, jsonify
+from flask_restful import Resource, Api
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from config import app, db, api
 from models import User
+
+app.secret_key = b'a\xdb\xd2\x13\x93\xc1\xe9\x97\xef2\xe3\x004U\xd1Z'
+api = Api(app)
 
 class ClearSession(Resource):
 
@@ -16,28 +20,51 @@ class ClearSession(Resource):
         return {}, 204
 
 class Signup(Resource):
-    
-    def post(self):
-        json = request.get_json()
-        user = User(
-            username=json['username']
-        )
-        user.password_hash = json['password']
-        db.session.add(user)
-        db.session.commit()
-        return user.to_dict(), 201
+
+    def post(self): 
+        password = request.get_json()['password']
+        username = request.get_json()['username']
+        if username and password:
+            new_user = User(username=username)
+            new_user.password_hash = password
+            db.session.add(new_user)
+            db.session.commit()
+            session['user_id'] = new_user.id
+            return new_user.to_dict(), 201
+        return {'error': '422 Unprocessable Entity'}, 422
 
 class CheckSession(Resource):
-    pass
+
+    def get(self):
+        if session.get('user_id'):
+            user = User.query.filter(User.id == session['user_id']).first()
+            return user.to_dict(), 200
+        return {}, 204
 
 class Login(Resource):
-    pass
+    def post(self):
+        password = request.get_json()['password']
+        username = request.get_json()['username']
+        user = User.query.filter(User.username == username).first()
+        if user.authenticate(password):
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        return {'error': '401 Unauthorized'}, 401
 
 class Logout(Resource):
-    pass
+      def delete(self):
+          if 'user_id' in session:
+              session.clear()
+              return {}, 204
+          return {"error": "No active session found"}, 400
+
+
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Login, "/login", endpoint="login")
+api.add_resource(Logout, "/logout", endpoint="logout")
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
